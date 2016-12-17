badef24e3e22b04c3a8a7cb918e82d9d65f601e7
uhconst <uryelhenrique.c@gmail.com>
2016-12-17 20:36:18 -0200

    Completando PessoaDao

--- PetCare System/src/model/dao/PessoaDao.java
+++ PetCare System/src/model/dao/PessoaDao.java
@@ -5,10 +5,59 @@
  */
 package model.dao;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import model.domain.Pessoa;
+
 /**
  *
  * @author Constancio
  */
 public class PessoaDao{
+    
+    public void salvarAtualizar(Pessoa pessoa) {
+        EntityManager em = Conexao.getEntityManager();
+        em.getTransaction().begin();
+        if(pessoa.getId() !=null){
+            pessoa = em.merge(pessoa);
+        }
+        em.persist(pessoa);
+        em.getTransaction().commit();
+        em.close();
+    }
+    
+    public void excluir(Pessoa pessoa) {
+        EntityManager em = Conexao.getEntityManager();
+        em.getTransaction().begin();
+        pessoa = em.merge(pessoa);
+        em.remove(pessoa);
+        em.getTransaction().commit();
+        em.close();
+    }
+    
+    public List<Pessoa> pesquisar(Pessoa pessoa) {
+        EntityManager em = Conexao.getEntityManager();
+        StringBuilder sql = new StringBuilder("from Pessoa c "
+                + "where 1 = 1 ");
+        if (pessoa.getId() !=null) {
+            sql.append("and c.id = :idpessoa ");
+        }
+        if (pessoa.getNome() !=null &&
+                !pessoa.getNome().equals("")){
+            sql.append("and c.nome like :nome");
+            
+        }
+        Query query = em.createQuery(sql.toString());
+        if (pessoa.getId() !=null) {
+            query.setParameter("idpessoa",pessoa.getId());
+            
+        }
+        if (pessoa.getNome() != null &&
+                !pessoa.getNome().equals("")){
+            query.setParameter("nome","%"+pessoa.getNome());
+        }
+        return query.getResultList();
+    }
     
 }
